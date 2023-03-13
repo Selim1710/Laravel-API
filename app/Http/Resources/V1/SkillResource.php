fa1426b59de3deaeeb2942f26bfbeb53a404863f
Selim1710 <shamimaldeen@gmail.com>
2023-03-13 10:47:08 +0600

    api CRUD completed

---
+++ app/Http/Resources/V1/SkillResource.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Resources\V1;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class SkillResource extends JsonResource
+{
+    public function toArray($request)
+    {
+        return[
+            'id'=>$this->id,
+            'name'=>$this->name,
+            'url'=>$this->slug,
+        ];
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Http\Controllers\Api\V1\SkillController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes - http://127.0.0.1:8000/api/v1/skills/create
+|--------------------------------------------------------------------------
+|
+*/
+
+Route::group(['prefix'=>'v1'], function(){
+    
+    Route::resource('skills', SkillController::class);
+
+});
